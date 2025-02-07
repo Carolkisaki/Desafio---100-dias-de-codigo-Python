26a656b74b7b7a458981c4f1917b4e19396800f1
Carolina Kisaki <119511712+Carolkisaki@users.noreply.github.com>
2025-02-06 21:16:09 -0300

    Create VariaveisEOperadores.py

---
+++ dia01/VariaveisEOperadores.py
@@ -0,0 +1,10 @@
+idade = 30
+print(idade)
+
+cliente1, cliente2, cliente3 = "Bob", "Maria", "Ana"
+print(cliente1)
+
+nome = "Carolina"
+sobrenome = "Kisaki"
+Nome_completo = nome + " " + sobrenome
+print(Nome_completo)
